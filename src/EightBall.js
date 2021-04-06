6717280e68c756df17ad0c1725ba1f2773a23f79
David Zemel <zemel96@gmail.com>
2021-04-06 15:02:57 -0400

    finished FS 1

--- src/EightBall.js
+++ src/EightBall.js
@@ -1,25 +1,57 @@
-import React, {useState} from "react";
-import {getRandomMsg, getRandomMsgIndex} from "./helpers";
+import React, { useState } from "react";
+import { getRandomMsg, getRandomMsgIndex } from "./helpers";
 
 function EightBall(props) {
-  const {answers} = props;
+  const { answers } = props;
   const [color, setColor] = useState("black");
   const [text, setText] = useState("Think of a Question");
+  const [greenCount, setGreen] = useState(0);
+  const [redCount, setRed] = useState(0);
+  const [yellowCount, setYellow] = useState(0);
 
   function handleClick(evt) {
     evt.preventDefault();
     const [msg, color] = getRandomMsg(answers);
+    getColor(color);
     setText(msg);
     setColor(color);
   }
 
+  function getColor(color) {
+    if (color === "green") {
+      setGreen(greenCount + 1);
+    } else if (color === "goldenrod") {
+      setYellow(yellowCount + 1);
+    } else if (color === "red") {
+      setRed(redCount + 1);
+    }
+  }
+
+  function resetEightBall(evt) {
+    evt.preventDefault();
+    setText("Think of a Question");
+    setColor("black");
+  }
+
   return (
     <div className="Eightball-container">
-      <button className="Eightball" style={{backgroundColor: color}} onClick ={handleClick}>
-          <span className="Eightball-text">{text}</span>
+      <button
+        className="Eightball"
+        style={{ backgroundColor: color }}
+        onClick={handleClick}
+      >
+        <span className="Eightball-text">{text}</span>
       </button>
+      <button className="Eightball-reset" onClick={resetEightBall}>
+        Reset
+      </button>
+      <ul>
+        <li>Red Color Count: {redCount}</li>
+        <li>Yellow Color Count: {yellowCount}</li>
+        <li>Green Color Count: {greenCount}</li>
+      </ul>
     </div>
-  )
+  );
 }
 
 EightBall.defaultProps = {
@@ -44,7 +76,7 @@ EightBall.defaultProps = {
     { msg: "My sources say no.", color: "red" },
     { msg: "Outlook not so good.", color: "red" },
     { msg: "Very doubtful.", color: "red" },
-  ]
-}
+  ],
+};
 
 export default EightBall;
